1af886242a0e5901cb848117e3b53bb81b2f9acf
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-29 22:01:11 +0530

    Parantheses

--- Day - 2/Assignment - 3/Solution.java
+++ Day - 2/Assignment - 3/Solution.java
@@ -1,6 +1,20 @@
-class Solution{
+import java.util.Stack;
+
+public class Solution{
 	public static String isMatching(String str){
 		// fill you code Here
+		Stack st = new Stack();
+		for(int i = 0; i < str.length(); i++){
+			if((str.charAt(i) == '{') || (str.charAt(i) == '(') || (str.charAt(i) == '[')){
+				st.push(str.charAt(i));
+			}
+
+			if((str.charAt(i) == '}') || (str.charAt(i) == ')') || (str.charAt(i) == ']')){
+				if(st.isEmpty()){
+					return "NO";
+				}
+			}
+		}
 		return "YES";
 	}
 }
