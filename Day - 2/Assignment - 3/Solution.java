010fbba3d54215f9739f0a72ad19da6a81f8e1c4
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-29 16:04:40 +0530

    Day 2

---
+++ Day - 2/Assignment - 3/Solution.java
@@ -0,0 +1,6 @@
+class Solution{
+	public static String isMatching(String str){
+		// fill you code Here
+		return "YES";
+	}
+}
---
+++ Day - 2/Assignment - 4/Solution.java
@@ -0,0 +1,6 @@
+class Solution{
+	public static String Josephus(int a, int b){
+		// fill you code Here
+		return "";
+	}
+}
