3981a0fd2ae940f0b6304a7c455805c539a5810c
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-10-30 14:04:19 +0530

    Sorting

--- Day - 3/Assignment - 3/Solution.java
+++ Day - 3/Assignment - 3/Solution.java
@@ -3,24 +3,25 @@ import java.util.*;
 public class Solution{
 	public int[] sortInsertion(int[] arr){
 		for(int i = 0; i < arr.length; i++){
-			int min = i;
-			for(int j = i + 1; j < arr.length; j++){
-				if(a[j] < a[min]){
-					min = j;
-				}
+			for(int j = i; j > 0 && (arr[j] < arr[j - 1]); j--){
+				int temp = arr[j];
+				arr[j] = arr[j-1];
+				arr[j - 1] = temp;
 			}
-			int temp = arr[i];
-			a[i] = a[min];
-			a[min] = temp;
 		}
 		return arr;
 	}
 	public int[] sortSelection(int[] arr){
 		for(int i = 0; i < arr.length; i++){
 			int min = i;
-			for(int j = i; j > 0 && (arr[j] < arr[j - 1]); j--){
-				
+			for(int j = i + 1; j < arr.length; j++){
+				if(arr[j] < arr[min]){
+					min = j;
+				}
 			}
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
 		}
 		return arr;
 	}
