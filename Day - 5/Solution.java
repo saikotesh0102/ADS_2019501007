2d7122f8a229a828475273f3964ef0cc1d400429
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-11-01 08:54:44 +0530

    Day - 5

--- Day - 5/Solution.java
+++ Day - 5/Solution.java
@@ -61,6 +61,7 @@ public class Solution {
 		int BC_count = Integer.parseInt(sc.nextLine());
 		int SC_count = Integer.parseInt(sc.nextLine());
 		int ST_count = Integer.parseInt(sc.nextLine());
+		sc.close();
 
 		Selection sel_obj = new Selection(no_of_Students, no_of_positions, open, BC_count,SC_count,ST_count);
 		for(int i = 0; i < no_of_Students; i++){
@@ -74,6 +75,7 @@ public class Solution {
 	}
 	public static void checkOutput(Student[] arr, int no_of_positions, String outputFile) throws Exception{
 		Scanner sc = new Scanner(new File(outputFile));
+		sc.close();
 		for(int i = 0; i < no_of_positions; i++){
 			if(!sc.nextLine().equals(arr[i].name)){
 				System.out.println("Your output is not matching with output in file "+ outputFile);
@@ -81,7 +83,6 @@ public class Solution {
 			}
 		}
 		System.out.println("your output with file "+ outputFile+" Matched. This Test case passed");
-
 	}
 
 }
