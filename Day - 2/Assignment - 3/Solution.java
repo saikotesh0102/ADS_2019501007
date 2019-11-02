1f1ac9d130e4439ad32d0757239df8f0f8454e86
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-11-02 12:53:13 +0530

    Parantheses

--- Day - 2/Assignment - 3/Solution.java
+++ Day - 2/Assignment - 3/Solution.java
@@ -1,7 +1,15 @@
-public class Solution{
+/**
+ * @author SaiKotesh0102
+ */
 
+public class Solution{
 	StackNode head = null;
 	int size = 0;
+	/**
+	 * Time Complexity : O(N)
+	 * @param str
+	 * @return String of Yes or No
+	 */
 	public static String isMatching(String str){
 		Solution sol = new Solution();
 		String[] temp = str.split("");
@@ -14,7 +22,11 @@ public class Solution{
 		} 
 		return "NO";
 	}
-
+	/**
+	 * Method to push the element into Stack
+	 * Time Complexity : O(1)
+	 * @param data
+	 */
     public void push(final String data) {
 		if (size == 0) {
 			StackNode firstNode = new StackNode(data);
