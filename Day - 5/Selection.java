bfe706936e99ca35197757151cb0942aebcfa672
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-11-03 23:14:43 +0530

    Student Reservatioon

--- Day - 5/Selection.java
+++ Day - 5/Selection.java
@@ -18,52 +18,81 @@ public class Selection{
     }
 
 	public Student[] getSelectedList(){
-        for(int i = 0; i < list.length ; i++){
+        getSortedList(list);
+
+		Student[] selected = new Student[vacancies];
+		int position = 0;
+		// int open = this.open;
+		// int bc = this.bcReserved;
+		// int sc = this.scReserved;
+		// int st = this.stReserved;
+		// int vac = this.vacancies;
+
+		for (int i = 0; i < open; i++) {
+			selected[position++] = list[i];
+			list[i] = null;
+			open--;
+			vacancies--;
+		}
+
+		for (int j = 0; j < list.length; j++) {
+			if (list[j] == null) {
+				continue;
+			}
+			if (list[j].getStudentCategory().equals("BC")) {
+				if (bcReserved > 0) {
+					selected[position++] = list[j];
+					list[j] = null;
+					bcReserved--;
+					vacancies--;
+				}
+			} else if (list[j].getStudentCategory().equals("SC")) {
+				if (scReserved > 0) {
+					selected[position++] = list[j];
+					list[j] = null;
+					scReserved--;
+					vacancies--;
+				}
+			} else if (list[j].getStudentCategory().equals("ST")) {
+				if (stReserved > 0) {
+					selected[position++] = list[j];
+					list[j] = null;
+					stReserved--;
+					vacancies--;
+				}
+			}
+		}
+		
+		if (vacancies > 0) {
+			for (int j = 0; j < list.length; j++) {
+				if (list[j] == null) {
+					continue;
+				}
+ 				selected[position++] = list[j];
+				vacancies--;
+				if (vacancies == 0) {
+					break;
+				}
+			}
+        }
+        
+        getSortedList(selected);
+        return selected;
+    }
+
+    public Student[] getSortedList(Student[] Students){
+
+        for(int i = 0; i < Students.length ; i++){
             int max = i;
-            for(int j = i; j < list.length; j++){
-                if(list[j].compareTo(list[max]) == 1){
+            for(int j = i; j < Students.length; j++){
+                if(Students[j].compareTo(Students[max]) == 1){
                     max = j;
                 }
             }
-            Student temp = list[i];
-            list[i] = list[max];
-            list[max] = temp;
+            Student temp = Students[i];
+            Students[i] = Students[max];
+            Students[max] = temp;
         }
-		return list;
-    }
-
-    public students[] studentsAlloted(){
-        Students[] studentsAlloted = new Student[vacancies];
-        int count = 0;
-        for(int i = 0; i < list.length && open > 0 && vacancies > 0; i++){
-            studentsAlloted.add(list[i]);
-            open--;
-            vacancies--;
-            count++;
-        }
-
-        for (int i = count; i < list.length && vacancies > 0; i++) {
-            if (list[i].getStudentCategory().equals("BC") && bcReserved > 0) {
-                studentsAlloted.add(list[i]);
-                bcReserved--;
-                count++;
-                vacancies--;
-            }
-            
-            if (list[i].getStudentCategory().equals("SC") && scReserved > 0) {
-                studentsAlloted.add(list[i]);
-                scReserved--;
-                count++;
-                vacancies--;
-            }
-
-            if (list[i].getStudentCategory().equals("ST") && stReserved > 0) {
-                studentsAlloted.add(list[i]);
-                stReserved--;
-                count++;
-                vacancies--;
-            }
-        }
-        return studentsAlloted;
+		return Students;
     }
 }
