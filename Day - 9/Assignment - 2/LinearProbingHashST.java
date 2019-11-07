21e9eb4fd8fc6d6ffa26b8406df8477134bf4a15
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-11-07 17:38:28 +0530

    Linear Probing Hash Table

---
+++ Day - 9/Assignment - 2/LinearProbingHashST.java
@@ -0,0 +1,115 @@
+import java.util.*;
+
+public class LinearProbingHashST<Key extends Comparable<Key>, Value>{
+    private static final int INIT_CAPACITY = 4;
+    private int n;           // number of key-value pairs in the symbol table
+    private int m;           // size of linear probing table
+    private Key[] keys;      // the keys
+    private Value[] vals;    // the values
+
+    public LinearProbingHashST() {
+        this(INIT_CAPACITY);
+    }
+
+    public LinearProbingHashST(int capacity) {
+        this.m = capacity;
+        this.n = 0;
+        this.keys = (Key[])   new Comparable[m];
+        this.vals = (Value[]) new Comparable[m];
+    }
+
+    public void put(Key key, Value val) {
+        if (val == null) {
+            delete(key);
+            return;
+        }
+
+        // double table size if 50% full
+        if (n >= m/2) resize(2*m);
+
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % m) {
+            if (keys[i].equals(key)) {
+                vals[i] = val;
+                return;
+            }
+        }
+        keys[i] = key;
+        vals[i] = val;
+        n++;
+    }
+
+    public Value get(Key key) {
+        for (int i = hash(key); keys[i] != null; i = (i + 1) % m){
+            if (keys[i].equals(key)){
+                return vals[i];
+            }
+        }
+        return null;
+    }
+
+    public void delete(Key key) {
+        if (!contains(key)) return;
+        // find position i of key
+        int i = hash(key);
+        while (!key.equals(keys[i])) {
+            i = (i + 1) % m;
+        }
+
+        // delete key and associated value
+        keys[i] = null;
+        vals[i] = null;
+
+        // rehash all keys in same cluster
+        i = (i + 1) % m;
+        while (keys[i] != null) {
+            // delete keys[i] an vals[i] and reinsert
+            Key   keyToRehash = keys[i];
+            Value valToRehash = vals[i];
+            keys[i] = null;
+            vals[i] = null;
+            n--;
+            put(keyToRehash, valToRehash);
+            i = (i + 1) % m;
+        }
+        n--;
+        // halves size of array if it's 12.5% full or less
+        if (n > 0 && n <= m/8){
+            resize(m/2);
+        }
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    public boolean contains(Key key) {
+        return get(key) != null;
+    }
+
+    private int hash(Key key) {
+        return (key.hashCode() & 0x7fffffff) % m;
+    }
+
+    private void resize(int capacity) {
+        LinearProbingHashST<Key, Value> temp = new LinearProbingHashST<Key, Value>(capacity);
+        for (int i = 0; i < m; i++) {
+            if (keys[i] != null) {
+                temp.put(keys[i], vals[i]);
+            }
+        }
+        keys = temp.keys;
+        vals = temp.vals;
+        m    = temp.m;
+    }
+
+    public static void main(String[] args) { 
+        LinearProbingHashST<String, Integer> st = new LinearProbingHashST<String, Integer>();
+        st.put("Sai",1);
+        System.out.println(st.get("Sai"));
+    }
+}
