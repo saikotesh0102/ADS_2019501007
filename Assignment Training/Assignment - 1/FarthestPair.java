81c59f0cb76fb2fd562bd101b85f2aec5c6056ed
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-11-14 14:08:01 +0530

    Assignment - 1

---
+++ Assignment Training/Assignment - 1/FarthestPair.java
@@ -0,0 +1,27 @@
+/**
+ * @aut
+ */
+
+public class FarthestPair{
+
+    public String farthestPair(double[] arr){
+        double max = arr[arr.length - 1];
+        double min = arr[0];
+        for(int i = 0; i < arr.length ; i++){
+            if(arr[i] < min){
+                min = arr[i];
+            }
+
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        return "[" + max + ","+ min + "]";
+    }
+
+    public static void main(String[] args) {
+        FarthestPair fp = new FarthestPair();
+        double[] arr = {10.0,5.1,2.0,26.0,100.0,90.0};
+        System.out.println(fp.farthestPair(arr));
+    }
+}
