ff2a93eee0a94f48eaddd92071a816125a09ad30
saikotesh0102 <saikotesh101104@msitprogram.net>
2019-11-22 17:03:18 +0530

    Percolation

--- Project - 3/Percolation.java
+++ Project - 3/Percolation.java
@@ -1,3 +1,93 @@
+/**
+ * @author SaiKotesh0102
+ */
+
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class Percolation{
-    
+    private boolean[][] grid;
+    private int start;
+    private int end;
+    private int size;
+    private int sitesOpen;
+    private WeightedQuickUnionUF wq;
+
+    public Percolation(int n){
+        if(n <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        size = n;
+        end = size * size + 1;
+        wq = new WeightedQuickUnionUF(size * size + 2);
+        grid = new boolean[size][size];
+    }
+
+    public void open(int row, int col){
+        if (row > 0 && row <= size && col > 0 && col <= size) {
+            if(grid[row-1][col-1]){
+                return;
+            }
+            grid[row - 1][col - 1] = true;
+            sitesOpen = sitesOpen + 1;
+
+            if (row == 1) {
+                wq.union(get1DPosition(row, col), start);
+            }
+
+            if (row == size) {
+                wq.union(get1DPosition(row, col), end);
+            }
+
+            if (col > 1 && isOpen(row, col - 1)) {
+                wq.union(get1DPosition(row, col), get1DPosition(row, col - 1));
+            }
+
+            if (col < size && isOpen(row, col + 1)) {
+                wq.union(get1DPosition(row, col), get1DPosition(row, col + 1));
+            }
+
+            if (row > 1 && isOpen(row - 1, col)) {
+                wq.union(get1DPosition(row, col), get1DPosition(row - 1, col));
+            }
+
+            if (row < size && isOpen(row + 1, col)) {
+                wq.union(get1DPosition(row, col), get1DPosition(row + 1, col));
+            }
+        }else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int get1DPosition(int row, int col) {
+        return size * (row - 1) + col;
+    }
+
+    public boolean isOpen(int i, int j) {
+        if (i > 0 && i <= size && j > 0 && j <= size) {
+            return grid[i-1][j-1];
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public int numberOfOpenSites(){
+        return sitesOpen;
+    }
+
+    public boolean isFull(int i, int j) {
+        if (i > 0 && i <= size && j > 0 && j <= size) {
+            return wq.connected(start, get1DPosition(i , j));
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public boolean percolates() {
+        return wq.connected(start, end);
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
--- Project - 3/PercolationStats.java
+++ Project - 3/PercolationStats.java
@@ -1,3 +1,61 @@
+/**
+ * @author SaiKotesh0102
+ */
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
 public class PercolationStats{
-    
+    private Percolation per;
+    private double[] values;
+    private int count;
+    private double mean;
+    private double stdDev;
+
+    public double mean(){
+        mean = StdStats.mean(values);
+        return mean;
+    }
+
+    public double stddev(){
+        stdDev = StdStats.stddev(values);
+        return stdDev;
+    }
+
+    public double confidenceHi(){
+        return mean + ((1.96 * stdDev) / Math.sqrt(count));
+    }
+
+    public double confidenceLo(){
+        return mean - ((1.96 * stdDev) / Math.sqrt(count));
+    }
+
+    public PercolationStats(int n, int trials){
+        if(n <= 0 || trials <= 0){
+            throw new IllegalArgumentException();
+        }
+
+        count = trials;
+        values = new double[count];
+        for (int i = 0; i < count; i++) {
+            per = new Percolation(n);
+            int sitesOpen = 0;
+
+            while(! per.percolates()){
+                int j = StdRandom.uniform(1, n + 1);
+                int k = StdRandom.uniform(1, n + 1);
+
+                if (!per.isOpen(j, k)) {
+                    per.open(j, k);
+                    sitesOpen++;
+                }
+            }
+            double value = (double) sitesOpen/(n * n);
+            values[i] = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
